1705fb17a9b25de7ca153f61259e703cf91cba7e
Kyoseung Koo <iam@kyo.seung.info>
2014-01-28 12:37:48 +0900

    Use jquery-modal and add first popup
    Use jquery-modal and add first popup.
    Change something.
    (this source code is trash)

--- js/ef.js
+++ js/ef.js
@@ -1,26 +1,46 @@
 // Must include 'jquery-2.1.0.min.js'
-
-var isPopUp = false;
-
+// Must include 'jquery-modal.min.js'
 $(document).ready(function() {
-	$("#applyButton").click(function() {
-		isPopUp = true;
-		$( "#popUp" ).css( "display", "block" );
-		$( "#popUp" ).css( "width", $(window).width() );
-		$( "#popUp" ).css( "height", $(window).height() );
-		$( "#popUp" ).load( "./html/popUp_1.html" );
-	});
-	$("#popUp").click(function() {
-		isPopUp = false;
-		$( "#popUp" ).css( "display", "none" );
-		$( "#popUp" ).empty();
-	});
-});
+	// '데이트 신청하기' 버튼 클릭시
+	$('#applyButton').click(function(){
+		$('#popUp').load('../html/popUp_1.html', function() {
+			$('#popUp').modal();
+			$('#closeModal').css('top', '-275px' );
+			$('#closeModal').css('right', '-40px' );
 
-$(window).resize(function (){
-	if( isPopUp == true )
-	{
-		$( "#popUp" ).css( "width", $(window).width() );
-		$( "#popUp" ).css( "height", $(window).height() );
-	}
+			// '데이트 신청하기' 팝업에서 이미지 클릭 시
+			$('#popUp_1_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "146px");
+				$('#popUp_gift_border').css('left', "24px");
+			});
+			$('#popUp_2_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "146px");
+				$('#popUp_gift_border').css('left', "302px");
+			});
+			$('#popUp_3_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "314px");
+				$('#popUp_gift_border').css('left', "24px");
+			});
+			$('#popUp_4_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "314px");
+				$('#popUp_gift_border').css('left', "302px");
+			});
+
+			// '경품 선택하기' 후 넘어가는 팝업
+			$('#applyPopup1_selectButton').click(function(){
+				$('#popUp').empty();
+				$('#popUp').load('../html/popUp_2.html', function() {
+					$('#popUp').modal();
+					
+				});
+			});
+			
+		});
+	});
+	
+
 });
