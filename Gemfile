af8fef0ccff05f192684eca28b1bc8e4ee261aa3
Kyoseung Koo <iam@kyo.seung.info>
2014-01-29 13:46:20 +0900

    Merge onesup:master
    Merge onesup:master

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+gem 'rails', '4.1.0.beta1'
+gem 'sqlite3'
+gem 'mysql2'
+gem 'sass-rails', '~> 4.0.0.rc1'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'sdoc',               group: :doc, require: false
+gem 'spring',             group: :development
+gem 'unicorn'
+gem 'capistrano-rails',   group: :development
+gem 'capistrano-rbenv',   group: :development
+gem 'simple_form'
+gem 'devise'
+gem 'guard-rails',        group: :development
+gem 'pry',                group: :development
+gem 'quiet_assets',       group: :development
+gem 'rspec-rails'
+gem 'factory_girl_rails'
+group :test do
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem "database_cleaner", '~> 1.2.0'
+  gem "email_spec"
+end
---
+++ public/js/ef.js
@@ -0,0 +1,77 @@
+// Must include 'jquery-2.1.0.min.js'
+// Must include 'jquery-modal.min.js'
+$(document).ready(function() {
+	// '데이트 신청하기' 버튼 클릭시
+	$('#applyButton').click(function(){
+		$('#popUp').load('../html/popUp_1.html', function() {
+			$('#popUp').modal();
+			$('#closeModal').css('top', '-305px' );
+			$('#closeModal').css('right', '-40px' );
+
+			// '데이트 신청하기' 팝업에서 이미지 클릭 시
+			$('#popUp_1_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "146px");
+				$('#popUp_gift_border').css('left', "24px");
+			});
+			$('#popUp_2_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "146px");
+				$('#popUp_gift_border').css('left', "302px");
+			});
+			$('#popUp_3_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "314px");
+				$('#popUp_gift_border').css('left', "24px");
+			});
+			$('#popUp_4_gi').click(function(){
+				$('#popUp_gift_border').css('display', 'block');
+				$('#popUp_gift_border').css('top', "314px");
+				$('#popUp_gift_border').css('left', "302px");
+			});
+
+			// '경품 선택하기' 후 넘어가는 팝업
+			$('#applyPopup1_selectButton').click(function(){
+				$('#popUp').empty();
+				$('#popUp').load('../html/popUp_2.html', function() {
+					$('#popUp').modal();
+					$('#closeModal').css('top', '-391px' );
+					$('#closeModal').css('right', '-46px' );
+					// '문자 보내기' 후 넘어가는 팝업
+					$('#applyPopup2_sendButton').click(function(){
+						$('#popUp').empty();
+						$('#popUp').load('../html/popUp_3.html', function() {
+							$('#popUp').modal();
+							$('#closeModal').css('top', '-349px' );
+							$('#closeModal').css('right', '-46px' );
+							// '완료' 후 넘어가는 팝업
+							$('#applyPopup3_finButton').click(function(){
+								$('#popUp').empty();
+								$('#popUp').load('../html/popUp_4.html', function() {
+									$('#popUp').modal();
+									$('#closeModal').css('top', '-150px' );
+									$('#closeModal').css('right', '-46px' );
+									// '완료' 후 넘어가는 팝업
+									$('#applyPopup4_finButton').click(function(){
+										$('#popUp').empty();
+										/*
+										$('#popUp').load('../html/popUp_5.html', function() {
+											$('#popUp').modal();
+											$('#closeModal').css('top', '-349px' );
+											$('#closeModal').css('right', '-46px' );
+											applyPopup4_finButton
+										});
+										*/
+									});
+								});
+							});
+						});
+					});
+				});
+			});
+			
+		});
+	});
+	
+
+});
