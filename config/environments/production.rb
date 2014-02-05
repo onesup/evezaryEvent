b67b6d28103d64ef48174dc2065f4ef57713ad00
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-05 18:10:50 +0900

    scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compress = true
  
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :scss
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
