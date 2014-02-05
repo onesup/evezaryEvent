24d5a5d1dc392ec2317095e0c566f719cb05e593
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-05 17:03:21 +0900

    asset compress

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module EvezaryEvent
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.enabled = true
-    config.assets.js_compressor  = :uglifier
-    config.assets.css_compressor = :sass
     config.time_zone = 'Asia/Seoul'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
