3d25be3050962f154079f9b985f4384b0a377eab
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-05 11:11:30 +0900

    application config

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,16 @@ module EvezaryEvent
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
+    config.time_zone = 'Asia/Seoul'
+    config.assets.enabled = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += [
+      'application.js', 'application.css', 
+      'admin.js', 'admin.css', 
+      'web.css', 'web.js',
+      'mobile.css'
+    ]
   end
 end
