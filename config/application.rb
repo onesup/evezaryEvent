dfba426d264063d9fef7659235fe69e70d744265
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-05 15:26:44 +0900

    deploy

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module EvezaryEvent
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.time_zone = 'Asia/Seoul'
     config.assets.enabled = true
+    config.assets.js_compressor  = :uglifier
+    config.assets.css_compressor = :sass
+    config.time_zone = 'Asia/Seoul'
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
