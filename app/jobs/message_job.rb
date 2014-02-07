3f116ef5c260821c9a4606c7d930a13a731b905f
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-07 21:38:13 +0900

    messaging fix

--- app/jobs/message_job.rb
+++ app/jobs/message_job.rb
@@ -2,10 +2,10 @@ class MessageJob
   include SuckerPunch::Job
 
   def perform(message, send_message)
-    message.send_mms(message, send_message)
+    message.send_mms(send_message)
   end
   
   def later(sec, message, send_message)
-    after(sec) { perform(message, send_message) }
+    after(sec) { perform(send_message) }
   end
 end
