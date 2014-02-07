1217ec328985bc81eb1f7d185bed6012b480040c
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-07 19:36:27 +0900

    양쪽 다 문자 보내기

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,11 +10,21 @@ class MessagesController < ApplicationController
     @message.sent_at = Time.now
     @message.send_phone = send_phone unless send_phone == "--"
     @message.dest_phone = dest_phone unless dest_phone == "--"
+    dest_phone_copy = @message.dest_phone
+    msg_body_copy = @message.msg_body
     respond_to do |format|
       if @message.save
-
-
+        @message.msg_body = @message.msg_body + to_mom(@message.store)
+        binding.pry
         MessageJob.new.async.perform(@message)
+        @message.msg_body = to_user(@message.store)
+        @message.dest_phone = @message.send_phone
+        binding.pry
+        MessageJob.new.async.perform(@message)
+        @message.msg_body = msg_body_copy
+        @message.dest_phone = dest_phone_copy
+        @message.save
+
         format.json { render json: {result: @message.result, id: @message.id}, status: :created }
         format.html { redirect_to mobile_apply_2_path(send_phone_hash), notice: 'User was successfully updated.' }
       else
@@ -50,23 +60,33 @@ class MessagesController < ApplicationController
     end
     
     def store_info(store)
-      store.
+      store.title+"
+      "+store.address+"
+      "+store.phone
     end
     
-    def to_mother
-      "엄마 이불 어때?
+    def to_mom(store)
+      "
+      엄마 이불 어때?
       오랜만에 데이트도 하면서 
       이브자리로 이불 고르러
       같이 가요!
       
-      매장위치"
+      매장위치"+"
+      "+store_info(store)
     end
     
-    def to_user
+    def to_user(store)
       "지금 엄마와 함께 
       혼수이불 고르러 
       이브자리로 오세요! 
-      이불은 만져보고 골라야죠!"
+      이불은 만져보고 골라야죠!
+      "+ store_info(store) +"
+      소문내기 이벤트에 참여하시면 
+      당첨확률이 높아집니다
+      https://event3.evezary.co.kr
+      
+      * 당첨자 확인을 꼭 해주세요!"
     end
   
 end
