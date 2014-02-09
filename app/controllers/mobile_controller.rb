e4191ed02aa6506c5dce0c69190302f029ca69a1
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-09 10:27:29 +0900

    모바일 문자 보내기 완성

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -5,11 +5,15 @@ class MobileController < ApplicationController
     lng = params["lng"]
     i = 1
     length = 0
-    while length < 4
-      puts "@@search stores: "+length.to_s
-      @stores = Store.near([lat,lng], i)
-      length = @stores.length
-      i += 1
+    unless lat.nil?
+      while length < 4
+        puts "@@search stores: "+length.to_s
+        @stores = Store.near([lat,lng], i)
+        length = @stores.length
+        i += 1
+      end
+    else
+      @stores = Store.near([37.5146212,127.0554681],5)
     end
   end
   
@@ -17,9 +21,27 @@ class MobileController < ApplicationController
   end
   
   def search_stores
+    @cache = {
+      myPhone_1: params[:myPhone_1],
+      myPhone_2: params[:myPhone_2],
+      myPhone_3: params[:myPhone_3],
+      momPhone_1: params[:momPhone_1],
+      momPhone_2: params[:momPhone_2],
+      momPhone_3: params[:momPhone_3],
+      ta: params[:ta]
+    }
   end
   
   def apply_1
+    @cache = {
+      myPhone_1: params[:myPhone_1],
+      myPhone_2: params[:myPhone_2],
+      myPhone_3: params[:myPhone_3],
+      momPhone_1: params[:momPhone_1],
+      momPhone_2: params[:momPhone_2],
+      momPhone_3: params[:momPhone_3],
+      ta: params[:ta]
+    }
   end
   
   def apply_2
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
 
-  get "mobile_index" => "mobile#index"
-  get "mobile_apply_1" => "mobile#apply_1"
-  get "mobile_apply_2" => "mobile#apply_2"
-  get "mobile_apply_3" => "mobile#apply_3"
-  get "mobile_search_stores" => "mobile#search_stores"
-  get "mobile_person" => "mobile#person"
-  get "near_stores" => "mobile#near_stores"
-  get "home_index" => "home#index"
-  get "home_popup_1" => "home#popUp_1"
-  get "home_popup_2" => "home#popUp_2"
-  get "home_popup_3" => "home#popUp_3"
-  get "home_popup_4" => "home#popUp_4"
-  get "home_popup_5" => "home#popUp_5"
-  get "home_popup_6" => "home#popUp_6"
-  get "home_popup_blog" => "home#popUp_blog"
-  get "blog_image_download" => "home#download_image"
+  get "mobile_index"          => "mobile#index"
+  get "mobile_apply_1"        => "mobile#apply_1"
+  get "mobile_apply_2"        => "mobile#apply_2"
+  get "mobile_apply_3"        => "mobile#apply_3"
+  get "mobile_search_stores"  => "mobile#search_stores"
+  get "mobile_person"         => "mobile#person"
+  get "near_stores"           => "mobile#near_stores"
+  get "home_index"            => "home#index"
+  get "home_popup_1"          => "home#popUp_1"
+  get "home_popup_2"          => "home#popUp_2"
+  get "home_popup_3"          => "home#popUp_3"
+  get "home_popup_4"          => "home#popUp_4"
+  get "home_popup_5"          => "home#popUp_5"
+  get "home_popup_6"          => "home#popUp_6"
+  get "home_popup_blog"       => "home#popUp_blog"
+  get "blog_image_download"   => "home#download_image"
   get '/:id', to: 'users#tracking_log', constraints: { id: /e.+-\d.+/ }
   devise_for :users
   resources :users, only: [:create, :update] do
@@ -24,7 +24,9 @@ Rails.application.routes.draw do
     end
   end
   resources :messages, only: [:create, :update]
-  
+  namespace :mobile do
+    resources :messages, only: [:create]
+  end
   namespace :admin do
     resources :users
     resources :messages
