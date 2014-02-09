703c539d49d881074c0c357fc258cc9de67f1694
Koo Kyoseung <iam@kyo.seung.info>
2014-02-09 20:14:44 +0900

    add admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,12 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  layout 'admin'
+  
   def index
+    @users = User.order("id desc")
   end
 
-  def new
+  def show
+    @user = User.find(params[:id])
   end
 end
