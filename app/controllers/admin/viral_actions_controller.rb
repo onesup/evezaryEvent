3169dcd4b114746409f7d499e9c8b3dd37bc00e3
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-13 19:40:34 +0900

    admin 바이럴 참여 현황

--- app/controllers/admin/viral_actions_controller.rb
+++ app/controllers/admin/viral_actions_controller.rb
@@ -1,2 +1,12 @@
 class Admin::ViralActionsController < ApplicationController
+  before_action :authenticate_user!
+  layout 'admin'
+  
+  def index
+    @actions = ViralAction.order("id desc")
+  end
+
+  def show
+    @actions = ViralAction.find(params[:id])
+  end
 end
