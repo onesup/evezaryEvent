49c02ac9432f50bc1059c491faad76451d51c210
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-14 20:50:45 +0900

    admin users view 변경

--- app/models/viral_action.rb
+++ app/models/viral_action.rb
@@ -1,3 +1,9 @@
 class ViralAction < ActiveRecord::Base
   belongs_to :user
+  scope :web, -> { where(device: "web") }
+  scope :mobile, -> { where(device: "mobile") }
+  scope :kakao_talk, -> { where(platform: "kakao_talk") }
+  scope :kakao_story, -> { where(platform: "kakao_story") }
+  scope :facebook, -> { where(platform: "facebook") }
+  scope :blog, -> { where(platform: "blog") }
 end
