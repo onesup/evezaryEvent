d0407d57be8dd86d2d67426fe15edffb9123d12b
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-14 11:05:42 +0900

    mobile 로딩시 get_location 삭제
    불필요한 api 호출 최소화

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,13 @@ class Store < ActiveRecord::Base
   include DailyCount
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
+  
+  def self.address_to_coord(address)
+    address = "목동"
+    key = Rails.application.secrets.naver_map_key
+    query_url = "http://openapi.map.naver.com/api/geocode.php?encoding=utf-8&coord=LatLng&key=" + key
+    # query_url = query_url + "&query=" + address
+    resource = RestClient.get query_url, query: address
+    # result = resource.get 
+  end
 end
