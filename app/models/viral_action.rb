edc320ed7a4cff3e4b0b9afdf13064a20eb760db
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-17 12:23:19 +0900

    viral action 수정

--- app/models/viral_action.rb
+++ app/models/viral_action.rb
@@ -1,6 +1,6 @@
 class ViralAction < ActiveRecord::Base
   belongs_to :user
-  scope :web, -> { where(device: "web") }
+  scope :pc, -> { where(device: "pc") }
   scope :mobile, -> { where(device: "mobile") }
   scope :kakao_talk, -> { where(platform: "kakao_talk") }
   scope :kakao_story, -> { where(platform: "kakao_story") }
