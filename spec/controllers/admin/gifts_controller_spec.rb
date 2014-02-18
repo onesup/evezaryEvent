8c040d3e65a95dedded8151e259ce11c9a93cb75
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-02-18 11:06:02 +0900

    테스팅 초기화

--- spec/controllers/admin/gifts_controller_spec.rb
+++ spec/controllers/admin/gifts_controller_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe Admin::GiftsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
+    # it "returns http success" do
+    #   get 'index'
+    #   response.should be_success
+    # end
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
+    # it "returns http success" do
+    #   get 'show'
+    #   response.should be_success
+    # end
   end
 
 end
