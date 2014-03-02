7354634d8f7448473c6e2e97ea485217bbe6aa7d
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-02 22:51:18 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.1.0'
 set :application, 'evezaryEvent'
 set :repo_url, 'git@github.com:onesup/evezaryEvent.git'
 set :scm_user, "onesup"
-
+set :branch, "popup"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
