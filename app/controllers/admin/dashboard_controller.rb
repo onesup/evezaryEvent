652efc32598006469d18fcbee76242fb9ae1a640
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-05 18:30:53 +0900

    어드민 대시보드 날짜 조절

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   layout 'admin'
   
   def index
-    @beginning_of_event = DateTime.parse("2014-02-01 00:00:00")
+    # @beginning_of_event = DateTime.parse("2014-02-01 00:00:00")
+    @beginning_of_event = (Time.now - 14.days).beginning_of_day
   end
 end
