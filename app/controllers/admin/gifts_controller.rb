8cf1d7885bd43ed48819a8751ae4adbcd96a63d7
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-12 16:10:19 +0900

    당첨자 지역 표시

--- app/controllers/admin/gifts_controller.rb
+++ app/controllers/admin/gifts_controller.rb
@@ -7,7 +7,8 @@ class Admin::GiftsController < ApplicationController
 
   def show
     @gift = Gift.find params[:id]
-    @users = @gift.users.order("viral_score desc").limit 200
+    @users = @gift.users.order("viral_score desc").limit 300
+    # @seoul_users = @gift.users.order("viral_score desc").limit 200
   end
   
 end
